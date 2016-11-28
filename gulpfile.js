b73517ea2d160a323a952f14db59f8d1ccc8fc70
Léo <leo@e-frogg.fr>
2016-11-28 08:34:00 +0100

    add : default task watcher

--- gulpfile.js
+++ gulpfile.js
@@ -5,26 +5,23 @@ const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const watch = require('gulp-watch');
 
-gulp.task('default', function () {
-    // Callback mode, useful if any plugin in the pipeline depends on the `end`/`flush` event
-    var n = 0;
-    var gulpify = function() {
-        try {
-            gulp.src('src/**/*.js')
-                .pipe(sourcemaps.init())
-                .pipe(babel({
-                    presets: ['es2015']
-                }))
-                .pipe(concat('all.js'))
-                .pipe(uglify())
-                .pipe(sourcemaps.write('.'))
-                .pipe(gulp.dest('dist'));
-        } catch(e) {
-            console.log(e);
-        }
-        console.log("gulpified "+(n++));
-    };
 
-    gulpify();
-    return watch('src/**/*.js',gulpify) ;
+
+gulp.task('js', function () {
+  return gulp.src('src/**/*.js')
+          .pipe(sourcemaps.init())
+          .pipe(babel({
+              presets: ['es2015']
+          }))
+          .pipe(concat('all.js'))
+          .pipe(uglify())
+          .pipe(sourcemaps.write('.'))
+          .pipe(gulp.dest('dist'));
+});
+
+
+
+gulp.task('default', ['js'], function () {
+
+    return gulp.watch(['src/**/*.js'],['js']);
 });
